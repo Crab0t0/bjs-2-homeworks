d0c7ca2c4f72830156b19645deb3234dc094f054
Crab0t0 <crabottoo@gmail.com>
2026-04-14 21:44:00 +0300

    решение

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
+  return arr1.length === arr2.length &&
+         arr1.every((value, index) => value === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  const result = users
+    .filter(user => user.gender === gender) 
+    .map(user => user.age) 
+    .reduce((acc, age, index, arr) => {
+      acc += age;
+      if (index === arr.length - 1) {
+        return acc / arr.length; 
+      }
+      return acc;
+    }, 0);
+
+  return result;
 }
+
